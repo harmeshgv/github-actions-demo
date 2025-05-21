c3bb72bc16c4918a76a3ee45b0aa78f257830bf5
HARMESH095 <harmeshgopinathan@gmail.com>
2025-05-21 12:19:10 +0530

    Fix GitHub Actions test import error

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,11 +1,10 @@
 from app.calculator import add, subtract, multiply
 
 def test_add():
-    assert add(2,4)==6
-    
+    assert add(2, 4) == 6
+
 def test_subtract():
-    assert subtract(4,2)==2
-    
+    assert subtract(4, 2) == 2
+
 def test_multiply():
-    assert multiply(3,6)==18
-    
+    assert multiply(3, 6) == 18
