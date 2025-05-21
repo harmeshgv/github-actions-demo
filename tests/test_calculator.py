ffcdad9c08052275d81b9cbd2b5d413451f1754b
HARMESH095 <harmeshgopinathan@gmail.com>
2025-05-21 12:01:13 +0530

    ADDED additional method multiplication

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,8 +1,11 @@
-from app.calculator import add, subtract
+from app.calculator import add, subtract, multiply
 
 def test_add():
     assert add(2,4)==6
     
 def test_subtract():
     assert subtract(4,2)==2
+    
+def test_multiply():
+    assert multiply(3,6)==18
     
